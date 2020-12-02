a16b2b2c74ad1e2a9bf53a7660a6c72fd70d3c21
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-02 15:11:35 +0100

    Changing color for previous tz

--- config/seeds/timezones.js
+++ config/seeds/timezones.js
@@ -14,20 +14,20 @@ var timezones = [
 	{timezone: "Africa/Addis_Ababa", country_code: "ET", moment: moment.tz("Africa/Addis_Ababa"), color: "#ffdf00"},
 
 	//Antarctica timezones...
-	{timezone: "Antarctica/Casey", country_code: "AQ", moment: moment.tz("Antarctica/Casey"), color: "#7FFF00"},
-	{timezone: "Antarctica/Davis", country_code: "AQ", moment: moment.tz("Antarctica/Davis"), color: "#7FFF00"},
-	{timezone: "Antarctica/DumontDUrville", country_code: "AQ", moment: moment.tz("Antarctica/DumontDUrville"), color: "#7FFF00"},
-	{timezone: "Antarctica/Macquarie", country_code: "AU", moment: moment.tz("Antarctica/Macquarie"), color: "#7FFF00"},
-	{timezone: "Antarctica/Mawson", country_code: "AQ", moment: moment.tz("Antarctica/Mawson"), color: "#7FFF00"},
-	{timezone: "Antarctica/McMurdo", country_code: "AQ", moment: moment.tz("Antarctica/McMurdo"), color: "#7FFF00"},
-	{timezone: "Antarctica/Palmer", country_code: "AQ", moment: moment.tz("Antarctica/Palmer"), color: "#7FFF00"},
-	{timezone: "Antarctica/Rothera", country_code: "AQ", moment: moment.tz("Antarctica/Rothera"), color: "#7FFF00"},
-	{timezone: "Antarctica/Syowa", country_code: "AQ", moment: moment.tz("Antarctica/Syowa"), color: "#7FFF00"},
-	{timezone: "Antarctica/Troll", country_code: "AQ", moment: moment.tz("Antarctica/Troll"), color: "#7FFF00"},
-	{timezone: "Antarctica/Vostok", country_code: "AQ", moment: moment.tz("Antarctica/Vostok"), color: "#7FFF00"},
+	{timezone: "Antarctica/Casey", country_code: "AQ", moment: moment.tz("Antarctica/Casey"), color: "#FFC0CB"},
+	{timezone: "Antarctica/Davis", country_code: "AQ", moment: moment.tz("Antarctica/Davis"), color: "#FFC0CB"},
+	{timezone: "Antarctica/DumontDUrville", country_code: "AQ", moment: moment.tz("Antarctica/DumontDUrville"), color: "#FFC0CB"},
+	{timezone: "Antarctica/Macquarie", country_code: "AU", moment: moment.tz("Antarctica/Macquarie"), color: "#FFC0CB"},
+	{timezone: "Antarctica/Mawson", country_code: "AQ", moment: moment.tz("Antarctica/Mawson"), color: "#FFC0CB"},
+	{timezone: "Antarctica/McMurdo", country_code: "AQ", moment: moment.tz("Antarctica/McMurdo"), color: "#FFC0CB"},
+	{timezone: "Antarctica/Palmer", country_code: "AQ", moment: moment.tz("Antarctica/Palmer"), color: "#FFC0CB"},
+	{timezone: "Antarctica/Rothera", country_code: "AQ", moment: moment.tz("Antarctica/Rothera"), color: "#FFC0CB"},
+	{timezone: "Antarctica/Syowa", country_code: "AQ", moment: moment.tz("Antarctica/Syowa"), color: "#FFC0CB"},
+	{timezone: "Antarctica/Troll", country_code: "AQ", moment: moment.tz("Antarctica/Troll"), color: "#FFC0CB"},
+	{timezone: "Antarctica/Vostok", country_code: "AQ", moment: moment.tz("Antarctica/Vostok"), color: "#FFC0CB"},
 
 	//Arctic timezones...
-	{timezone: "Arctic/Longyearbyen", country_code: "SJ", moment: moment.tz("Arctic/Longyearbyen"), color: "#E7F9F3"},
+	{timezone: "Arctic/Longyearbyen", country_code: "SJ", moment: moment.tz("Arctic/Longyearbyen"), color: "#FD6C9E"},
 
 	//Atlantic timezones...
 	{timezone: "Atlantic/Azores", country_code: "PT", moment: moment.tz("Atlantic/Azores"), color: "#00FF00"},
