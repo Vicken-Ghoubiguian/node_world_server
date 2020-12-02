bcd6c7e4a2147f52541bd1e6bdb27391f97c9689
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-02 11:07:47 +0100

    Migration of arrays in 'modules' of 'config'

---
+++ config/modules/formats.js
@@ -0,0 +1,13 @@
+// Array containing all defined formats to display current date and time...
+var formats = [
+
+        {format: "DD/MM/YYYY HH:mm:ss", id: 0, selected: ""},
+        {format: "DD - MM - YYYY hh:mm:ss a", id: 1, selected: ""},
+        {format: "Do of MMMM hh:mm:ss a", id: 2, selected: ""},
+        {format: "dddd MMM YYYY HH:mm:ss", id: 3, selected: ""},
+        {format: "dddd MMMM YYYY HH:mm:ss", id: 4, selected: "selected"},
+        {format: "MMMM Do YYYY, hh:mm:ss a", id: 5, selected: ""}
+];
+
+//
+module.exports = formats;
---
+++ config/modules/selectionnableLanguages.js
@@ -0,0 +1,11 @@
+// Array containing all languages with relatives datas...
+var selectionnableLanguages = [
+
+        {value: "fr-fr", selected: "", text: "French"},
+        {value: "en-us", selected: "", text: "English"},
+        {value: "es-es", selected: "selected", text: "Spanish"},
+        {value: "ca", selected: "", text: "Catalan"}
+];
+
+//
+module.exports = selectionnableLanguages;
