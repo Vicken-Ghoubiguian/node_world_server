a72795cc778c001ccb6518220d08a50b93fb7b81
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-10 13:57:10 +0100

    Other things

--- config/modules/gitHub.js
+++ config/modules/gitHub.js
@@ -1,2 +1,32 @@
 //
 var axios = require('axios');
+
+// Definition of the 'getGitHub' function to get all datas of the '$userLogin' GitHub account...
+async function getGitHub($userLogin) {
+
+
+}
+
+// Definition of the 'getGitHubUsers' function to get all informations about the '$userLogin' user...
+async function getGitHubUsers($userLogin) {
+
+
+}
+
+// Definition of the 'getGitHubRepositories' function to get all the repositories owned by the '$userLogin' user...
+async function getGitHubRepositories($userLogin) {
+
+
+}
+
+// Definition of the 'getGitHubBranches' function to get all the branches in the '$reposName' repository owned by the '$userLogin' user...
+async function getGitHubBranches($userLogin, $reposName) {
+
+
+}
+
+// Definition of the 'getGitHubLanguages' function to get all the languages in the '$reposName' repository owned by the '$userLogin' user...
+async function getGitHubLanguages($userLogin, $reposName) {
+
+
+}
