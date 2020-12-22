dc3337b8a090dd73f39348e414732e9f43810428
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-22 11:46:54 +0100

    Adding new file for temperature units

---
+++ config/seeds/temperatureUnits.js
@@ -0,0 +1,10 @@
+// Array containing all temperature units with relatives datas...
+var selectionnableTemperatureUnits = [
+
+        {value: "Celsius", selected: ""},
+        {value: "Fahrenheit", selected: ""},
+        {value: "Kelvin", selected: "selected"}
+];
+
+// Export the 'selectionnableTemperatureUnits' array...
+module.exports = selectionnableTemperatureUnits;
