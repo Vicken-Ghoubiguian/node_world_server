cc3faab3e1f076286ab8a62cd095cf0fd2e72300
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-01-22 16:16:02 +0100

    Fixed issue for the 'Selectmenu'

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,40 +8,37 @@ $( function() {
 	$("#timezones_displayer").accordion();
 
 	// Triggering an action when an element of the 'choosen_language' <select> is chosen...
-	$("#choosen_language").change(function(){
+	$("#choosen_language").selectmenu({ change: function(event, ui){
 
 		// The form 'choosen_language_form' is then submitted with all of its datas...
 		$("#choosen_language_form").submit();
-	});
+	}});
 
 	// Triggering an action when an element of the 'second_choosen_language' <select> is chosen...
-	$("#second_choosen_language").change(function(){
+	$("#second_choosen_language").selectmenu({ change: function(event, ui){
 		
 		// The form 'second_choosen_language_form' is then submitted with all of its datas...
 		$("#second_choosen_language_form").submit();
-	});
+	}});
 
 	// Triggering an action when an element of the 'choosen_date_and_time_format' <select> is chosen...
-	$("#choosen_date_and_time_format").change(function(){
+	$("#choosen_date_and_time_format").selectmenu({ change: function(event, ui){
 
 		// The form 'choosen_format_form' is then submitted with all of its datas...
 		$("#choosen_format_form").submit();
-	});
+	}});
 
 	// Triggering an action when an element of the 'choosen_country_code' <select> is chosen...
-	$("#choosen_country_code").change(function(){
+	$("#choosen_country_code").selectmenu({ change: function(event, ui){
 
 		// The form 'choosen_country_code_form' is then submitted with all of its datas...
 		$("#choosen_country_code_form").submit();
-	});
+	}});
 
 	// Triggering an action when an element of the 'choosen_temperature_unit' <select> is chosen...
-	$("#choosen_temperature_unit").change(function(){
+	$("#choosen_temperature_unit").selectmenu({ change: function(event, ui){
 
 		// The form 'choosen_temperature_unit_form' is then submitted with all of its datas...
 		$("#choosen_temperature_unit_form").submit();
-	});
-
-	// Application of jQuery UI to selectboxes...
-	//$(".select_menu").selectmenu();
+	}});
 });
