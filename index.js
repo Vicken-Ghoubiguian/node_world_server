a2e2ad88a3d841a0dff6fde4fa94f37a4c90b588
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-01-22 18:28:25 +0100

    Writting 2 comments

--- index.js
+++ index.js
@@ -276,7 +276,7 @@ app.get('/', function(req, res) {
       }
     }
 
-    //
+    // Configuration of the 'currentCountryFlagURL' to get and display current country's flag...
     currentCountryFlagURL = "https://dynamospanish.com/wp-content/uploads/flags/images/" + currentCountry + "/1/small/" + currentCountry + ".png";
 
   	res.render('index.ejs', {"currentCountryFlagURL": currentCountryFlagURL, "selectionnableLanguages": selectionnableLanguages, "currentTemperatureUnitSymbol": currentTemperatureUnitSymbol, "fontAwesomeKit": fontAwesomeKit, "selectionnableTemperatureUnits": selectionnableTemperatureUnits, "countryCodeHashTable": countryCodeHashTable, "timezones": renderTimezonesArray, "currentDateAndTimeFormat": currentDateAndTimeFormat, "formats": formats, "weatherResults": results});
@@ -421,7 +421,7 @@ app.post('/', function(req, res) {
       }
     }
 
-    //
+    // Configuration of the 'currentCountryFlagURL' to get and display current country's flag...
     currentCountryFlagURL = "https://dynamospanish.com/wp-content/uploads/flags/images/" + currentCountry + "/1/small/" + currentCountry + ".png";
 
     res.render('index.ejs', {"currentCountryFlagURL": currentCountryFlagURL, "selectionnableLanguages": selectionnableLanguages, "currentTemperatureUnitSymbol": currentTemperatureUnitSymbol, "fontAwesomeKit": fontAwesomeKit, "selectionnableTemperatureUnits": selectionnableTemperatureUnits, "countryCodeHashTable": countryCodeHashTable, "timezones": renderTimezonesArray, "currentDateAndTimeFormat": currentDateAndTimeFormat, "formats": formats, "weatherResults": results});
