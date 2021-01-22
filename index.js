38a175eb3c5ac767bb20d67204abfd085fb89869
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-01-22 18:19:01 +0100

    Another step for integration

--- index.js
+++ index.js
@@ -277,7 +277,7 @@ app.get('/', function(req, res) {
     }
 
     //
-    currentCountryFlagURL = "https://dynamospanish.com/wp-content/uploads/flags/images/France/1/small/France.png"
+    currentCountryFlagURL = "https://dynamospanish.com/wp-content/uploads/flags/images/" + currentCountry + "/1/small/" + currentCountry + ".png";
 
   	res.render('index.ejs', {"currentCountryFlagURL": currentCountryFlagURL, "selectionnableLanguages": selectionnableLanguages, "currentTemperatureUnitSymbol": currentTemperatureUnitSymbol, "fontAwesomeKit": fontAwesomeKit, "selectionnableTemperatureUnits": selectionnableTemperatureUnits, "countryCodeHashTable": countryCodeHashTable, "timezones": renderTimezonesArray, "currentDateAndTimeFormat": currentDateAndTimeFormat, "formats": formats, "weatherResults": results});
     
@@ -422,7 +422,7 @@ app.post('/', function(req, res) {
     }
 
     //
-    currentCountryFlagURL = "https://dynamospanish.com/wp-content/uploads/flags/images/France/1/small/France.png"
+    currentCountryFlagURL = "https://dynamospanish.com/wp-content/uploads/flags/images/" + currentCountry + "/1/small/" + currentCountry + ".png";
 
     res.render('index.ejs', {"currentCountryFlagURL": currentCountryFlagURL, "selectionnableLanguages": selectionnableLanguages, "currentTemperatureUnitSymbol": currentTemperatureUnitSymbol, "fontAwesomeKit": fontAwesomeKit, "selectionnableTemperatureUnits": selectionnableTemperatureUnits, "countryCodeHashTable": countryCodeHashTable, "timezones": renderTimezonesArray, "currentDateAndTimeFormat": currentDateAndTimeFormat, "formats": formats, "weatherResults": results});
 
