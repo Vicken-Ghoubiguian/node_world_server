ecd138513aaed7009f8e4a218b72a2466a67c8b4
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-01-24 17:23:37 +0100

    Yes it is

--- config/seeds/timezones.js
+++ config/seeds/timezones.js
@@ -186,7 +186,21 @@ function getCountryCodeFromTimezone(wishedTimezone) {
 function getCountryFromTimezone(wishedTimezone)
 {
 	// Definition of the 'wishedCountry' variable which will contain the found country code...
-	var wishedCountry = "France";
+	var wishedCountry = "";
+
+	// Browse for each element contained in the 'timezones' array...
+	for(var i = 0; i < timezones.length; i++)
+	{
+		// If the current element of the 'timezones' array corresponds to the desired timezone...
+		if(timezones[i].timezone === wishedTimezone) {
+
+			// Affectation of the found country code to the 'wishedCountry' variable...
+			wishedCountry = timezones[i].country;
+
+			// Leaving the loop...
+			break;
+		}
+	}
 
 	return wishedCountry;
 }
