d779987671c9ecb94210e3b9ceece30e3228a6c9
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-02-13 16:43:33 +0100

    New file for all pressure units as JSON datas

---
+++ config/seeds/pressureUnits.js
@@ -0,0 +1,10 @@
+// Array containing all pressure units with relatives datas...
+var selectionnablePressureUnits = [
+
+        {unit: "", id: 0, symbol: "", selected: ""},
+        {unit: "", id: 1, symbol: "", selected: ""},
+        {unit: "", id: 2, symbol: "", selected: ""}
+];
+
+// Export the 'selectionnablePressureUnits' array...
+module.exports = selectionnablePressureUnits;
