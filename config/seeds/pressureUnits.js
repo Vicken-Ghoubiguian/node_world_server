aeee8d3318a4f96fa4f601190e0c459f1fb6861c
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-02-14 08:10:23 +0100

    Adding all pressure units in JSON

--- config/seeds/pressureUnits.js
+++ config/seeds/pressureUnits.js
@@ -1,9 +1,12 @@
 // Array containing all pressure units with relatives datas...
 var selectionnablePressureUnits = [
 
-        {unit: "", id: 0, symbol: "", selected: ""},
-        {unit: "", id: 1, symbol: "", selected: ""},
-        {unit: "", id: 2, symbol: "", selected: ""}
+        {unit: "hectoPascal", id: 0, symbol: "hPa", selected: ""},
+        {unit: "Pascal", id: 1, symbol: "Pa", selected: ""},
+        {unit: "Bar", id: 2, symbol: "Bar", selected: ""},
+        {unit: "Atmosphere", id: 3, symbol: "Atm", selected: ""},
+        {unit: "Torr", id: 4, symbol: "Torr", selected: ""},
+        {unit: "Pounds per square inch", id: 5, symbol: "Psi", selected: ""}
 ];
 
 // Export the 'selectionnablePressureUnits' array...
