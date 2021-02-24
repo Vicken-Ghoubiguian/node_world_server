b9c707aaaef1d57b8f1d31bc030b2d405453b2ee
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-02-24 18:18:09 +0100

    Adding the 'name_format' field in the 'formats.js' file

--- config/seeds/formats.js
+++ config/seeds/formats.js
@@ -1,13 +1,13 @@
 // Array containing all defined formats to display current date and time...
 var formats = [
 
-        {id: 0, format: "DD/MM/YYYY HH:mm:ss", selected: ""},
-        {id: 1, format: "DD - MM - YYYY hh:mm:ss a", selected: ""},
-        {id: 2, format: "Do of MMMM hh:mm:ss a", selected: ""},
-        {id: 3, format: "dddd MMM YYYY HH:mm:ss", selected: ""},
-        {id: 4, format: "X", selected: ""},
-        {id: 5, format: "dddd MMMM YYYY HH:mm:ss", selected: ""},
-        {id: 6, format: "MMMM Do YYYY, hh:mm:ss a", selected: ""}
+        {id: 0, format: "DD/MM/YYYY HH:mm:ss", name_format: "DD/MM/YYYY HH:mm:ss", selected: ""},
+        {id: 1, format: "DD - MM - YYYY hh:mm:ss a", name_format: "DD - MM - YYYY hh:mm:ss a", selected: ""},
+        {id: 2, format: "Do of MMMM hh:mm:ss a", name_format: "Do of MMMM hh:mm:ss a", selected: ""},
+        {id: 3, format: "dddd MMM YYYY HH:mm:ss", name_format: "dddd MMM YYYY HH:mm:ss", selected: ""},
+        {id: 4, format: "X", name_format: "Timestamp", selected: ""},
+        {id: 5, format: "dddd MMMM YYYY HH:mm:ss", name_format: "dddd MMMM YYYY HH:mm:ss", selected: ""},
+        {id: 6, format: "MMMM Do YYYY, hh:mm:ss a", name_format: "MMMM Do YYYY, hh:mm:ss a", selected: ""}
 ];
 
 // Export the 'formats' array...
