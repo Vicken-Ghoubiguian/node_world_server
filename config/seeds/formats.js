c0b821f621c93c60b4efe985cb647bbd4630743b
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-02-24 18:13:05 +0100

    Switch places

--- config/seeds/formats.js
+++ config/seeds/formats.js
@@ -5,9 +5,9 @@ var formats = [
         {id: 1, format: "DD - MM - YYYY hh:mm:ss a", selected: ""},
         {id: 2, format: "Do of MMMM hh:mm:ss a", selected: ""},
         {id: 3, format: "dddd MMM YYYY HH:mm:ss", selected: ""},
-        {id: 4, format: "dddd MMMM YYYY HH:mm:ss", selected: ""},
-        {id: 5, format: "MMMM Do YYYY, hh:mm:ss a", selected: ""},
-        {id: 6, format: "X", selected: ""}
+        {id: 4, format: "X", selected: ""},
+        {id: 5, format: "dddd MMMM YYYY HH:mm:ss", selected: ""},
+        {id: 6, format: "MMMM Do YYYY, hh:mm:ss a", selected: ""}
 ];
 
 // Export the 'formats' array...
