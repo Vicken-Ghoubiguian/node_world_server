725c7911554328b70d2773ba649b50a21d2cbf79
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-03-07 16:41:35 +0100

    Font families JSON file

---
+++ config/seeds/fontFamilies.js
@@ -0,0 +1,9 @@
+// Array containing all accepted fonts with their name and URL...
+var fontFamilies = [
+
+	{id: 0, font: "Anurati", url: "http://fonts.cdnfonts.com/css/anurati"},
+	{id: 1, font: "Sofia", url: "https://fonts.googleapis.com/css?family=Sofia"}
+];
+
+// Export the 'fontFamilies' array...
+module.exports = fontFamilies;
