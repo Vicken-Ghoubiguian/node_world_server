0d4d8334d8e756bf737ec6349534e9d373e2976a
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-03-07 17:26:38 +0100

    Deployment of function from ff

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var currentTemperatureUnit = "Kelvin";
 var currentTemperatureUnitSymbol = "";
 var currentPressureUnit = "hectoPascal";
 var currentPressureUnitSymbol = "hPa";
-var currentFontFamily = ["Anurati", "http://fonts.cdnfonts.com/css/anurati"];
+var currentFontFamily = fontFamilies.getFontFamilyFromId(0);
 
 // Catching, treating and getting passed arguments to the 'passedArguments' constant...
 const passedArguments = require('minimist')(process.argv.slice(2));
