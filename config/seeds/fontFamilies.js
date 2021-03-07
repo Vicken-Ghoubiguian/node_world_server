5ac0804babab2a6a9cd65b51343d1a7e6bf73a51
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-03-07 17:22:39 +0100

    We are now :-))

--- config/seeds/fontFamilies.js
+++ config/seeds/fontFamilies.js
@@ -5,5 +5,32 @@ var fontFamilies = [
 	{id: 1, font: "Sofia", url: "https://fonts.googleapis.com/css?family=Sofia"}
 ];
 
+// Definition of the 'getFontFamilyFromId' function to identify and return all informations about font family identified by its id...
+function getFontFamilyFromId(wishedId) {
+
+	// Definition of the 'wishedFontFamily' array which will contain all informations about wished font family...
+	var wishedFontFamily = [];
+
+	// Browse for each element contained in the 'fontFamilies' array...
+	for(var i = 0; i < fontFamilies.length; i++)
+	{
+		// If the current element of the 'fontFamilies' array corresponds to the 'wishedId' value...
+		if(fontFamilies[i].id === wishedId) {
+
+			// Configuration of 'wishedFontFamily' array to return...
+			wishedFontFamily.push(fontFamilies[i].font);
+			wishedFontFamily.push(fontFamilies[i].url);
+
+			// Leaving the loop...
+			break;
+		}
+	}
+
+	return wishedFontFamily;
+}
+
 // Export the 'fontFamilies' array...
-module.exports = fontFamilies;
+module.exports = {
+	'fontFamilies': fontFamilies,
+	'getFontFamilyFromId': getFontFamilyFromId
+}
