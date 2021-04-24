f483f1c41e83d73d5a9f09d3e8e595ebbba11bf0
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-04-24 21:14:02 +0200

    Update of default familly font

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var currentTemperatureUnit = "Kelvin";
 var currentTemperatureUnitSymbol = "";
 var currentPressureUnit = "hectoPascal";
 var currentPressureUnitSymbol = "hPa";
-var currentFontFamily = fontFamilies.getFontFamilyFromId(20);
+var currentFontFamily = fontFamilies.getFontFamilyFromId(23);
 
 // Retrieving the array of font families...
 var fontFamiliesArray = fontFamilies.fontFamilies;
