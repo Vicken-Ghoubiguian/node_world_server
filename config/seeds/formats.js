326a7b927c3a69933d0dc667793fc0a555f06221
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-05-22 10:20:05 +0200

    My things of time of the hell of *¨*PM¨LLLMLKELDSQLS (destruction of the universe)

--- config/seeds/formats.js
+++ config/seeds/formats.js
@@ -7,7 +7,8 @@ var formats = [
         {id: 3, format: "dddd MMM YYYY HH:mm:ss", name_format: "dddd MMM YYYY HH:mm:ss", selected: ""},
         {id: 4, format: "X", name_format: "Timestamp", selected: ""},
         {id: 5, format: "dddd MMMM YYYY HH:mm:ss", name_format: "dddd MMMM YYYY HH:mm:ss", selected: ""},
-        {id: 6, format: "MMMM Do YYYY, hh:mm:ss a", name_format: "MMMM Do YYYY, hh:mm:ss a", selected: ""}
+        {id: 6, format: "MMMM Do YYYY, hh:mm:ss a", name_format: "MMMM Do YYYY, hh:mm:ss a", selected: ""},
+        {id: 7, format: "YYYY/MM/DD HH:mm:ss", name_format: "YYYY/MM/DD, HH:mm:ss", selected: ""}
 ];
 
 // Export the 'formats' array...
