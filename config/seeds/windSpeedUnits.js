e4840122b9fd90ee8dc004ec0e3e15d8998a6c65
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-06-19 10:40:03 +0200

    This is the new

--- config/seeds/windSpeedUnits.js
+++ config/seeds/windSpeedUnits.js
@@ -2,10 +2,12 @@
 var windSpeedUnits = [
 
 	{id: 0, format: "km/sec", name_format: "kilometer_per_second", selected: ""},
-	{id: 1, format: "m/sec", name_format: "meter_per_second", selected: ""},
-	{id: 2, format: "dm/sec", name_format: "decimeter_per_second", selected: ""},
-	{id: 3, format: "cm/sec", name_format: "centimeter_per_second", selected: ""},
-	{id: 4, format: "mm/sec", name_format: "millimeter_per_second", selected: ""}
+	{id: 1, format: "hm/sec", name_format: "hectometer_per_second", selected: ""},
+	{id: 2, format: "dam/sec", name_format: "decameter_per_second", selected: ""},
+	{id: 3, format: "m/sec", name_format: "meter_per_second", selected: ""},
+	{id: 4, format: "dm/sec", name_format: "decimeter_per_second", selected: ""},
+	{id: 5, format: "cm/sec", name_format: "centimeter_per_second", selected: ""},
+	{id: 6, format: "mm/sec", name_format: "millimeter_per_second", selected: ""}
 ];
 
 // Export the 'windSpeedUnits' array...
