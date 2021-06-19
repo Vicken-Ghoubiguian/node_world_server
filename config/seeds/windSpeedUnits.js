e37b809d666e119066958b2b21ac42a76cb18d7a
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-06-19 10:23:41 +0200

    Import newest things

--- config/seeds/windSpeedUnits.js
+++ config/seeds/windSpeedUnits.js
@@ -3,6 +3,8 @@ var windSpeedUnits = [
 
 	{id: 0, format: "m/sec", name_format: "meter_per_second", selected: ""},
 	{id: 1, format: "km/sec", name_format: "kilometer_per_second", selected: ""},
+	{id: 2, format: "cm/sec", name_format: "centimeter_per_second", selected: ""}
+	{id: 3, format: "mm/sec", name_format: "millimeter_per_second", selected: ""}
 ];
 
 // Export the 'windSpeedUnits' array...
