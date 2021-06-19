345642d1f7db279854e8345839b5263d92323955
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-06-19 10:06:56 +0200

    First element in array

--- config/seeds/windSpeedUnits.js
+++ config/seeds/windSpeedUnits.js
@@ -1,6 +1,7 @@
 // Array containing all defined units for wind speed...
 var windSpeedUnits = [
 
+	{id: 0, format: "m/sec", name_format: "meter_per_second", selected: ""}
 ];
 
 // Export the 'windSpeedUnits' array...
