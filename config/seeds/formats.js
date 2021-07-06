dd50db83c9e6b121b18ad4253b1a323a6cbb9b43
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-07-06 21:01:44 +0200

    M. format 5

--- config/seeds/formats.js
+++ config/seeds/formats.js
@@ -6,7 +6,7 @@ var formats = [
         {id: 2, format: "Do of MMMM hh:mm:ss a", name_format: "Do of MMMM hh:mm:ss a", selected: ""},
         {id: 3, format: "dddd MMM YYYY HH:mm:ss", name_format: "dddd MMM YYYY HH:mm:ss", selected: ""},
         {id: 4, format: "X", name_format: "Timestamp", selected: ""},
-        {id: 5, format: "dddd MMMM YYYY HH:mm:ss", name_format: "dddd MMMM YYYY HH:mm:ss", selected: ""},
+        {id: 5, format: "dddd DD MMMM YYYY HH:mm:ss", name_format: "dddd DD MMMM YYYY HH:mm:ss", selected: ""},
         {id: 6, format: "MMMM Do YYYY, hh:mm:ss a", name_format: "MMMM Do YYYY, hh:mm:ss a", selected: ""},
         {id: 7, format: "YYYY/MM/DD HH:mm:ss", name_format: "YYYY/MM/DD, HH:mm:ss", selected: ""}
 ];
